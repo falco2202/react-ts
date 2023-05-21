f9ac91c7ae3d210530bd2a6166eefe52bfe4f2bf
falco2202 <giaont2001@gmail.com>
2023-05-21 13:27:10 +0700

    Implement redux toolkit, redux saga and multilanguage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
 import '@/assets/styles/index.scss'
+import { BrowserRouter } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@/locales'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
+  </React.StrictMode>
 )
